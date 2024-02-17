a60f7204cffe4808e63d02ec235629940e7c5fca
likuor <likuor0528@gmail.com>
2024-02-17 09:08:16 -0800

    add: doubly linked lists

--- dataStructure/doublyLinkedLists.js
+++ dataStructure/doublyLinkedLists.js
@@ -2,27 +2,31 @@ class Node {
   constructor(val) {
     this.val = val;
     this.next = null;
+    this.prev = null;
   }
 }
 
-class SinglyLinkedLists {
+class DublyLinkedLists {
   constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
   }
+
   push(val) {
     let newNode = new Node(val);
     if (!this.head) {
       this.head = newNode;
-      this.tail = this.head;
+      this.tail = newNode;
     } else {
       this.tail.next = newNode;
+      newNode.prev = this.tail;
       this.tail = newNode;
     }
     this.length++;
     return this;
   }
+
   pop() {
     if (!this.head) return undefined;
     let current = this.head;
@@ -132,7 +136,7 @@ class SinglyLinkedLists {
   }
 }
 
-let list = new SinglyLinkedLists();
+let list = new DublyLinkedLists();
 list.push('Hi');
 list.push('test');
 list.push('You');
@@ -140,7 +144,8 @@ list.push('You');
 // console.log(list.shift());
 // console.log(list.shift());
 // console.log(list.shift());
-console.log('============', list.reverse());
+// console.log('============', list.reverse());
+console.log('============', list);
 
 // console.log(list.shift());
 // console.log(list.shift());
