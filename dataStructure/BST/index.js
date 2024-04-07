c6fd039ae75f11b7855506db9b2110a872e45465
likuor <likuor0528@gmail.com>
2024-04-07 10:08:42 -0700

    add: BFS

--- dataStructure/BST/index.js
+++ dataStructure/BST/index.js
@@ -51,18 +51,38 @@ class BinarySearchTree {
     }
     return false;
   }
+
+  BFS() {
+    let node = this.root,
+      data = [],
+      queue = [];
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
 }
 
 let tree = new BinarySearchTree();
+// tree.insert(10);
+// tree.insert(5);
+// tree.insert(13);
+// tree.insert(11);
+// tree.insert(2);
+// tree.insert(16);
+// tree.insert(7);
+// tree.insert(3);
 tree.insert(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(11);
-tree.insert(2);
-tree.insert(16);
-tree.insert(7);
+tree.insert(6);
+tree.insert(15);
 tree.insert(3);
-// tree.contains(3);
+tree.insert(8);
+tree.insert(20);
+console.log(tree.BFS());
 // console.log('tree', tree.contains(10));
 // console.log('tree', tree.contains(16));
 // console.log(tree.root);
