dc50f2cdf4ab778d5f7632affdb86009aed50220
likuor <likuor0528@gmail.com>
2024-04-14 12:24:48 -0700

    add: heap remove

--- dataStructure/BinaryHeaps/MaxBinaryHeap/index.js
+++ dataStructure/BinaryHeaps/MaxBinaryHeap/index.js
@@ -1,6 +1,6 @@
 class MaxBinaryHeap {
   constructor() {
-    this.values = [41, 39, 33, 18, 27, 12];
+    this.values = [];
   }
 
   insert(val) {
@@ -20,13 +20,64 @@ class MaxBinaryHeap {
       index = parentIndex;
     }
   }
+
+  extractMax() {
+    if (this.values.length === 0) return undefined;
+    if (this.values.length === 1) return this.values.pop();
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+
+  sinkDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > element) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+
+      if (swap === null) break;
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap();
 
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
 heap.insert(55);
-heap.insert(1);
-heap.insert(45);
-heap.insert(199);
+
+heap.extractMax();
 
 console.log(heap.values);
